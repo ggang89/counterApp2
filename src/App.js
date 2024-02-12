67eec6ca23465747ddf4a827317d3f822e9ab9a2
ggang89 <ggang89u@gmail.com>
2024-02-12 17:03:15 +0900

    counter ui 생성(기본 css)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div>
+      <h1 className="headtitle">COUNTER</h1>
+
+      <div className="count">
+        <button>-1</button>
+        <p className="countText">0</p>
+        <button>+1</button>
+      </div>
       
     </div>
   );
