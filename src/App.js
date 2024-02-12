88cb726c794994488e7efd04798f12c256874272
ggang89 <ggang89u@gmail.com>
2024-02-12 17:57:13 +0900

    state를 사용해서 {변수}와 onClick이벤트 를 set함수로 연결해줌 => [변수명, set변경함수]= useState(기본값); 형식주의!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import "./App.css";
+import {useState} from "react";
 
 function App() {
+
+  const [count,setCount] = useState(0);
+  const handleMinus =()=>{
+    setCount(count -1);
+  }
+  const handlePlus =()=>{
+    setCount(count +1);
+  }
   return (
     <div>
       <h1 className="headtitle">COUNTER</h1>
 
-      <div className="count">
-        <button>-1</button>
-        <p className="countText">0</p>
-        <button>+1</button>
+      <div className="countBox">
+        <button onClick={handleMinus}>-1</button>
+        <p className="countText">{count}</p>
+        <button onClick={handlePlus}>+1</button>
       </div>
       
+
     </div>
   );
 }
